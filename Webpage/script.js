7b488478897a74c4db0fe31d4f844027e129c2d9
Tommy <TommyCohn116@gmail.com>
2016-06-15 17:37:37 -0400

    Do a little message formatting on the webpage.

--- Webpage/script.js
+++ Webpage/script.js
@@ -4,7 +4,7 @@ var ws = new WebSocket("ws://127.0.0.1:12345/");
 
 //Define a function to be executed when a message is received
 ws.onmessage = function (event) {
-    var message = event.data; //Store the message data to a variable
+    var message = formatMessage(event.data); //Store the message data to a variable
     document.body.innerHTML = message + "<br>" + document.body.innerHTML; //Add it to the top of the webpage
 };
 
@@ -15,4 +15,13 @@ function sendDataRequest() {
 
 ws.onopen = function() {
 	requestAnimationFrame(sendDataRequest); //This is to help keep the browser from getting lost and hanging
+}
+
+function formatMessage(raw) {
+	var refined = raw.split("\n");
+	for(var i=0; i<refined.length; ++i) {
+		refined[i] = refined[i].replace(/\s/g, "");
+	}
+	console.log(refined);
+	return refined;
 }
